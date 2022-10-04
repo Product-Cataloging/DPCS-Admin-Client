e9e9354a3beb8846828b15190817099cd852c652
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-04 15:19:05 +0300

    generated category and category-form

--- src/app/operation/operation.module.ts
+++ src/app/operation/operation.module.ts
@@ -5,12 +5,16 @@ import { OperationRoutingModule } from './operation-routing.module';
 import { SupplierFormComponent } from './components/ui/supplier-form/supplier-form.component';
 import { SupplierComponent } from './components/container/supplier/supplier.component';
 import { AppCommonModule } from '../app.common.module';
+import { CategoryComponent } from './components/container/category/category.component';
+import { CategoryFormComponent } from './components/ui/category-form/category-form.component';
 
 
 @NgModule({
   declarations: [
     SupplierFormComponent,
-    SupplierComponent
+    SupplierComponent,
+    CategoryComponent,
+    CategoryFormComponent
   ],
   imports: [
     CommonModule,
