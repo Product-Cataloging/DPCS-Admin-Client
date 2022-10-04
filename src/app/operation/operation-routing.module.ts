0235b1e3194a7079deb1cf8d3f85c04536084e6e
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-04 15:37:11 +0300

    added routes for components inside operation module

--- src/app/operation/operation-routing.module.ts
+++ src/app/operation/operation-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './components/container/category/category.component';
+import { CurrencyComponent } from './components/container/currency/currency.component';
+import { CustomerComponent } from './components/container/customer/customer.component';
+import { ProductComponent } from './components/container/product/product.component';
 import { SupplierComponent } from './components/container/supplier/supplier.component';
+import { UnitOfMeasureComponent } from './components/container/unit-of-measure/unit-of-measure.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'suppliers', pathMatch: 'full' },
-  { path: 'suppliers', component: SupplierComponent }
+  { path: 'suppliers', component: SupplierComponent },
+  { path: 'categories', component: CategoryComponent },
+  { path: 'products', component: ProductComponent },
+  { path: 'customers', component: CustomerComponent },
+  { path: 'currencies', component: CurrencyComponent },
+  { path: 'units_of_measure', component: UnitOfMeasureComponent }
 ];
 
 @NgModule({
