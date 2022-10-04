72fa61a77bd65c6a77dd8a1e2e504b0eb7729d31
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-04 11:41:17 +0300

    generated footer component

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { LayoutComponent } from './components/container/layout/layout.component';
 import { HeaderComponent } from './components/ui/header/header.component';
+import { FooterComponent } from './components/ui/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
