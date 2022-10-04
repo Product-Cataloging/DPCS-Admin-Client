0b16ce3d84562b511988e68c420b6084cd850311
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-04 13:01:56 +0300

    generated supplier form component

--- src/app/operation/operation.module.ts
+++ src/app/operation/operation.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OperationRoutingModule } from './operation-routing.module';
+import { SupplierFormComponent } from './components/ui/supplier-form/supplier-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SupplierFormComponent
+  ],
   imports: [
     CommonModule,
     OperationRoutingModule
