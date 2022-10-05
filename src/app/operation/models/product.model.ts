63944809a634f8f400e0f8c5b78305c24847eb51
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-05 10:52:48 +0300

    generated product model and store

---
+++ src/app/operation/models/product.model.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  category_id: number;
+  supplier_id: number;
+}
+
+export function createProduct(params: Partial<Product>) {
+  return {
+
+  } as Product;
+}
