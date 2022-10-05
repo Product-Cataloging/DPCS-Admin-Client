ec1ae4b91eb3febef8d709cf89991a4908e884c3
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-05 11:33:49 +0300

    added mat-dialog to currency and currency-form

--- src/app/operation/components/container/currency/currency.component.ts
+++ src/app/operation/components/container/currency/currency.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CurrencyFormComponent } from '../../ui/currency-form/currency-form.component';
 
 @Component({
   selector: 'app-currency',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  onNewClick(): void {
+    const dialogRef = this.dialog.open(CurrencyFormComponent, {
+      width: '500px',
+      data: { id: null, name: '', alias: '' },
+    });
+
+    const submitForm = (dialogRef.componentInstance as any).submitForm.subscribe((data: any) => {
+      console.log('The dialog was submitted');
+      dialogRef.close();
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      submitForm.unsubscribe();
+    });
+  }
 }
--- src/app/operation/components/ui/currency-form/currency-form.component.ts
+++ src/app/operation/components/ui/currency-form/currency-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Currency } from 'src/app/operation/models/currency.model';
 
 @Component({
   selector: 'app-currency-form',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  @Output() submitForm = new EventEmitter();
+
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<CurrencyFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Currency) {
+    this.form = this.fb.group({
+      name: [this.data.name],
+      alias: [this.data.alias]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.submitForm.emit();
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
 }
