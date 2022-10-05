7085020764edcdff986a927aacea4b22cf33ac29
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-05 11:13:04 +0300

    connected category component with category-form-component using mat-dialog

--- src/app/operation/components/container/category/category.component.ts
+++ src/app/operation/components/container/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CategoryFormComponent } from '../../ui/category-form/category-form.component';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  onNewClick() {
+    const dialogRef = this.dialog.open(CategoryFormComponent, {
+      width: '500px',
+      data: { id: null, company_name: '' },
+    });
+
+    const submitForm = (dialogRef.componentInstance as any).submitForm.subscribe((data: any) => {
+      console.log('The dialog was submitted');
+      dialogRef.close();
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      submitForm.unsubscribe();
+    });
+  }
 }
--- src/app/operation/components/ui/category-form/category-form.component.ts
+++ src/app/operation/components/ui/category-form/category-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Category } from 'src/app/operation/models/category.model';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  @Output() submitForm = new EventEmitter();
+
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<CategoryFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Category
+  ) {
+    this.form = this.fb.group({
+      name: [this.data.name],
+      description: [this.data.description],
+      parent_id: [this.data.parent_id]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.submitForm.emit();
+  }
+
+  onClose() {
+    this.dialogRef.close();
   }
 
 }
