19c7505445d04aa909d6196ef4e58cfbba50d8cf
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-06 12:11:14 +0300

    added categories and suppliers dropdown to products form

--- src/app/operation/components/ui/product-form/product-form.component.ts
+++ src/app/operation/components/ui/product-form/product-form.component.ts
@@ -1,7 +1,14 @@
 import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/operation/models/category.model';
 import { Product } from 'src/app/operation/models/product.model';
+import { Supplier } from 'src/app/operation/models/supplier.model';
+import { CategoryQuery } from 'src/app/operation/state/category.query';
+import { CategoryService } from 'src/app/operation/state/category.service';
+import { SupplierQuery } from 'src/app/operation/state/supplier.query';
+import { SupplierService } from 'src/app/operation/state/supplier.service';
 
 @Component({
   selector: 'app-product-form',
@@ -13,7 +20,14 @@ export class ProductFormComponent implements OnInit {
   form: FormGroup;
   @Output() submitForm = new EventEmitter();
 
+  suppliers$: Observable<Supplier[]> = this.supplierQuery.selectAll();
+  categories$: Observable<Category[]> = this.categoryQuery.selectAll();
+
   constructor(
+    private categoryQuery: CategoryQuery,
+    private categoryService: CategoryService,
+    private supplierQuery: SupplierQuery,
+    private supplierService: SupplierService,
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<ProductFormComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Product
@@ -27,6 +41,8 @@ export class ProductFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.supplierService.get();
+    this.categoryService.get();
   }
 
   onSubmit() {
