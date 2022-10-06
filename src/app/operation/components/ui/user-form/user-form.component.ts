e5b817e2fa76aaaa3381f10ec320efed919ec4bb
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-06 17:46:48 +0300

    added user table, form and finished add edit functionality

--- src/app/operation/components/ui/user-form/user-form.component.ts
+++ src/app/operation/components/ui/user-form/user-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from 'src/app/operation/models/user.model';
 
 @Component({
   selector: 'app-user-form',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  mismatch: boolean = false;
+  @Output() submitForm = new EventEmitter();
+
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<UserFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: User
+  ) {
+    this.form = this.fb.group({
+      email: [this.data.email],
+      password: [this.data.password],
+      confirm_password: [this.data.password],
+      user_role: [this.data.user_role],
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if (this.form.value.password === this.form.value.confirm_password) {
+      this.mismatch = false;
+      delete this.form.value.confirm_password;
+      this.submitForm.emit(this.form.value);
+    } else {
+      this.mismatch = true;
+    }
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
 }
