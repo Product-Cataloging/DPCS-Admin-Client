ff79ca2135a881a6e12b80fb6ac9994b8f763e03
Abrham Alemayehu <aaabrham421@gmail.com>
2022-10-12 15:18:08 +0300

    finished service for quotation

--- src/app/admin/state/quotation.service.ts
+++ src/app/admin/state/quotation.service.ts
@@ -2,32 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ID } from '@datorama/akita';
 import { tap } from 'rxjs/operators';
+import { UtilService } from 'src/app/shared/services/util.service';
+import { environment } from 'src/environments/environment';
 import { Quotation } from '../models/quotation.model';
 import { QuotationStore } from './quotation.store';
 
 @Injectable({ providedIn: 'root' })
 export class QuotationService {
 
-  constructor(private quotationStore: QuotationStore, private http: HttpClient) {
+  constructor(private store: QuotationStore, private http: HttpClient) {
   }
-
 
   get() {
-    return this.http.get<Quotation[]>('https://api.com').pipe(tap(entities => {
-      this.quotationStore.set(entities);
-    }));
+    const url = `${environment.apiUrl}/quotations`;
+    return UtilService.get(url, this.http, this.store);
   }
 
-  add(quotation: Quotation) {
-    this.quotationStore.add(quotation);
-  }
+  // add(quotation: Quotation) {
+  //   this.store.add(quotation);
+  // }
 
-  update(id: number, quotation: Partial<Quotation>) {
-    this.quotationStore.update(id, quotation);
-  }
+  // update(id: number, quotation: Partial<Quotation>) {
+  //   this.store.update(id, quotation);
+  // }
 
-  remove(id: ID) {
-    this.quotationStore.remove(id);
-  }
+  // remove(id: ID) {
+  //   this.store.remove(id);
+  // }
 
 }
